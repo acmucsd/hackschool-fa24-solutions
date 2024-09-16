97fd12080f5b9fad22fe90589e46fd410fcac679
Angela Hu <angelahu925@gmail.com>
2024-09-16 16:50:03 -0700

    Fixing inconsistencies

--- server/controllers/activityController.js
+++ server/controllers/activityController.js
@@ -2,19 +2,27 @@ const Activities = require("../models/activityModel");
 
 // Create a GET async function to get all activities using the activity model schema
 const getActivity = async (req, res) => {
-    const activity = await Activities.find();
+    // Only get destination from trip
+    const activity = await Activities.find().populate('trip', 'destination');
     res.status(200).json(activity);
 }
 
 // Create a POST async function to add an activity using the activity model schema
 const postActivity = async (req, res) => {
-    const { destination, activitySpot, rating, review } = req.body;
+    const { trip, activitySpot, rating, review } = req.body;
 
-    if (!destination || !activitySpot || !rating) {
+    if (!trip || !activitySpot || !rating) {
         return res.status(400).json({ error: 'Invalid request' });
     }
 
-    const newActivity = await Trips.create(req.body);
+    const tripId = mongoose.Types.ObjectId.createFromHexString(trip);
+
+    const newActivity = await Activities.create({
+        trip: tripId,
+        activitySpot,
+        rating,
+        review
+    });
     res.status(200).json(newActivity);
 }
 
